7715ab3b7d8f3eb06627d9530f2ca47053366df2
jesterb0206 <jesterb@seattleu.edu>
2023-01-20 18:42:02 -0800

    Created Starter Files for the Required Components

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const index = () => {
+  return <div>Footer</div>;
+};
+
+export default index;
---
+++ src/Components/Project/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const index = () => {
+  return <div>Project</div>;
+};
+
+export default index;
