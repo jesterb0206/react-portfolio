48fd8020bf9db74922672bf9ae62dbaf61558f80
jesterb0206 <jesterb@seattleu.edu>
2023-01-22 03:02:07 -0800

    Updated Web App Details in the Portfolio Component

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -7,31 +7,66 @@ const Portfolio = () => {
       title: 'Art Cart',
       stack: 'MVC',
       image_url: 'https://i.imgur.com/7uqgR0b.png?1',
+      image_alt: 'Art Cart Website',
+      github_link: 'https://github.com/jesterb0206/art-cart',
+      deployed_link: 'https://art-cart-gui-guys-production.herokuapp.com/login',
     },
     {
       title: 'Book Search Engine',
       stack: 'MERN',
-      image_url: 'https://i.imgur.com/7uqgR0b.png?1',
+      image_url: 'https://i.imgur.com/pyMPrid.png',
+      image_alt: 'Book Search Engine Website',
+      github_link: 'https://github.com/jesterb0206/book-search-engine',
+      deployed_link: 'https://book-search-engine-0206.herokuapp.com/',
     },
     {
       title: 'Just Another Text Editor',
       stack: 'Express.js, JavaScript, Node.js, etc.',
-      image_url: 'https://i.imgur.com/7uqgR0b.png?1',
+      image_url: 'https://i.imgur.com/RNl813A.png',
+      image_alt: 'Just Another Text Editor Website',
+      github_link: 'https://github.com/jesterb0206/just-another-text-editor',
+      deployed_link: 'https://just-another-text-editor-0206.herokuapp.com/',
     },
     {
       title: 'Natours',
       stack: 'MVC',
-      image_url: 'https://i.imgur.com/7uqgR0b.png?1',
+      image_url: 'https://i.imgur.com/iJ1SCTj.png',
+      image_alt: 'Natours Website',
+      github_link: 'https://github.com/jesterb0206/natours',
+      deployed_link: 'https://natours-production-0a23.up.railway.app/',
     },
     {
       title: 'Tech Blog',
       stack: 'MVC',
-      image_url: 'https://i.imgur.com/7uqgR0b.png?1',
+      image_url: 'https://i.imgur.com/YmbUx0C.png',
+      image_alt: 'Tech Blog Website',
+      github_link: 'https://github.com/jesterb0206/tech-blog',
+      deployed_link: 'https://tech-blog-0206.herokuapp.com/',
     },
     {
       title: 'Today I Learned',
       stack: 'Node.js, React.js, etc.',
-      image_url: 'https://i.imgur.com/7uqgR0b.png?1',
+      image_url: 'https://i.imgur.com/KlZKarL.png',
+      image_alt: 'Today I Learned Website',
+      github_link: 'https://github.com/jesterb0206/today-i-learned',
+      deployed_link: 'https://today-i-learned-0206.netlify.app/',
+    },
+    {
+      title: 'Work Day Scheduler',
+      stack: 'Bootstrap, Moment.js, etc.',
+      image_url: 'https://i.imgur.com/3YiL2Tg.png',
+      image_alt: 'Work Day Scheduler Website',
+      github_link: 'https://github.com/jesterb0206/work-day-scheduler',
+      deployed_link: 'https://jesterb0206.github.io/work-day-scheduler/',
+    },
+    {
+      title: 'Weather Dashboard',
+      stack: 'CSS 3, HTML 5, JavaScript (ES6), OpenWeather API, etc.',
+      image_url: 'https://i.imgur.com/8GvPqvs.png',
+      image_alt: 'Weather Dashboard Website',
+      github_link: 'https://github.com/jesterb0206/server-side-apis-challenge',
+      deployed_link:
+        'https://jesterb0206.github.io/server-side-apis-challenge/',
     },
   ];
 
@@ -46,6 +81,9 @@ const Portfolio = () => {
               title={item.title}
               stack={item.stack}
               image_url={item.image_url}
+              image_alt={item.image_alt}
+              github_link={item.github_link}
+              deployed_link={item.deployed_link}
             />
           );
         })}
--- src/Components/Project/index.js
+++ src/Components/Project/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
-const Project = ({title, stack, image_url}) => {
+const Project = ({
+  title,
+  stack,
+  image_url,
+  image_alt,
+  github_link,
+  deployed_link,
+}) => {
   return (
-    <div id='single-project'>
-      <div id='project-details'>
-        <h1 id='project-title'>{title}</h1>
-        <p id='project-stack'>{stack}</p>
+    <div id='project-outer-wrapper'>
+      <div id='single-project'>
+        <div id='project-details'>
+          <h1 id='project-title'>{title}</h1>
+          <p id='project-stack'>{stack}</p>
+        </div>
+        <img id='project-image' src={image_url} alt={image_alt} />
       </div>
-      <img id='project-image' src={image_url} alt='Art Cart Products Page' />
+      <div id='project-links'>
+        <a target='_blank' rel='noreferrer noopener' href={github_link}>
+          GitHub Repository
+        </a>
+        <a target='_blank' rel='noreferrer noopener' href={deployed_link}>
+          Deployed Link
+        </a>
+      </div>
     </div>
   );
 };
