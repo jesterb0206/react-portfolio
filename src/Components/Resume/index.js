3a66e6d0bb44af0f04b8fb6959bb26f260f0b85d
jesterb0206 <jesterb@seattleu.edu>
2023-01-24 01:49:10 -0800

    Added SVG Logos to Resume Component

--- src/Components/Resume/index.js
+++ src/Components/Resume/index.js
@@ -30,6 +30,40 @@ const Resume = () => {
             <li>React.js</li>
             <li>Redux</li>
             <li>Tailwind CSS</li>
+            <img
+              src='https://skillicons.dev/icons?i=ae'
+              alt='Adobe Express Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=bootstrap'
+              alt='Bootstrap Logo'
+            ></img>
+            <img src='https://skillicons.dev/icons?i=css' alt='CSSLogo'></img>
+            <img
+              src='https://skillicons.dev/icons?i=html'
+              alt='HTML Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=js'
+              alt='JavaScript Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=jquery'
+              alt='jQuery Logo'
+            ></img>
+            <img src='https://skillicons.dev/icons?i=pug' alt='Pug Logo'></img>
+            <img
+              src='https://skillicons.dev/icons?i=react'
+              alt='React Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=redux'
+              alt='Redux Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=tailwind'
+              alt='Tailwind CSS Logo'
+            ></img>
           </ul>
         </div>
         <div id='dev-tools'>
@@ -37,19 +71,57 @@ const Resume = () => {
           <ul>
             <li>Babel</li>
             <li>Git</li>
+            <li>GitHub</li>
+            <li>GitLab</li>
             <li>Heroku</li>
             <li>Insomnia</li>
             <li>Jest</li>
+            <li>Markdown</li>
             <li>Netlify</li>
             <li>Postman</li>
             <li>Railway</li>
+            <li>Visual Studio Code</li>
             <li>Webpack</li>
+            <img src='https://skillicons.dev/icons?i=git' alt='Git Logo'></img>
+            <img
+              src='https://skillicons.dev/icons?i=github'
+              alt='GitHub Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=gitlab'
+              alt='GitLab Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=heroku'
+              alt='Heroku Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=jest'
+              alt='Jest Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=md'
+              alt='Markdown Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=netlify'
+              alt='Netlify Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=vscode'
+              alt='Visual Studio Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=webpack'
+              alt='Webpack Logo'
+            ></img>
           </ul>
         </div>
         <div id='backend'>
           <h1>Back End</h1>
           <ul>
             <li>Agile Development</li>
+            <li>Apollo</li>
             <li>Express.js</li>
             <li>GraphQL</li>
             <li>MERN Stack</li>
@@ -63,6 +135,34 @@ const Resume = () => {
             <li>Sequelize</li>
             <li>Supabase</li>
             <li>Test Driven Development</li>
+            <img
+              src='https://skillicons.dev/icons?i=apollo'
+              alt='Apollo Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=express'
+              alt='Express Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=graphql'
+              alt='GraphQL Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=mongodb'
+              alt='MongoDB Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=mysql'
+              alt='MySQL Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=nodejs'
+              alt='Node.js Logo'
+            ></img>
+            <img
+              src='https://skillicons.dev/icons?i=supabase'
+              alt='Supabase Logo'
+            ></img>
           </ul>
         </div>
       </div>
